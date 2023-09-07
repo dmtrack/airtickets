721a67adc743eb28ad25b3fd7723f47a4bc909a5
dmtrack <dmtrack.dev@gmail.com>
2023-09-07 18:23:04 +0500

    fix: date

--- src/App/utils/date.ts
+++ src/App/utils/date.ts
@@ -1,10 +1,12 @@
 export const dateFormat = (date: string) => {
-    const date_stamp = new Date(date)
+    const arrayToWork = date.split('.');
+    const dateToWork = `${arrayToWork[1]}.${arrayToWork[0]}.${arrayToWork[2]}`;
+    const date_stamp = new Date(dateToWork)
         .toLocaleDateString('ru-RU', {
             dateStyle: 'full',
         })
         .split(',');
-    const weekday = date_stamp[0].slice(0, 2);
+    const weekday = date_stamp[0].slice(0, 3);
     const dateContainer = date_stamp[1].split(' ');
     const day = dateContainer[1];
     const month = dateContainer[2].slice(0, 3);
